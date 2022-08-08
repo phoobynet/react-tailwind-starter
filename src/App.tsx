766361e45b040d86dc1991826fdf576c1f739b96
Glenn <phoobynet@users.noreply.github.com>
2022-08-08 13:59:47 +0100

    Cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Home from '@/pages/home/Home'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
 
-export default function App () {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
